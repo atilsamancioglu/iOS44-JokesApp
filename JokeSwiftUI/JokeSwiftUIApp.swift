52b323936517ccfeecd1c15e5008ffd1aeb24e4f
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-08-07 14:21:51 +0300

    Initial Commit

---
+++ JokeSwiftUI/JokeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeSwiftUIApp.swift
+//  JokeSwiftUI
+//
+//  Created by Atil Samancioglu on 7.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
